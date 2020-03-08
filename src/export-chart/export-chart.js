6e76e5aee6842090cf00c88612890771cb0e35a5
dabeng <dabeng413@gmail.com>
2020-03-08 11:41:46 +0800

    add export demo

--- src/export-chart/export-chart.js
+++ src/export-chart/export-chart.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import OrganizationChart from "../components/ChartContainer";
 import "./export-chart.css";
 
@@ -39,13 +39,53 @@ const ExportChart = () => {
   };
 
   const exportTo = () => {
-    orgchart.current.exportTo();
+    orgchart.current.exportTo(filename, fileextension);
+  };
+
+  const [filename, setFilename] = useState("organization_chart");
+  const [fileextension, setFileextension] = useState("png");
+
+  const onNameChange = event => {
+    setFilename(event.target.value);
+  };
+  const onExtensionChange = event => {
+    setFileextension(event.target.value);
   };
 
   return (
     <>
       <section className="toolbar">
-        <button className="btn-export" onClick={exportTo}>
+        <label htmlFor="txt-filename">Filename:</label>
+        <input
+          id="txt-filename"
+          type="text"
+          value={filename}
+          onChange={onNameChange}
+          style={{ fontSize: "1rem", marginRight: "2rem" }}
+        />
+        <span>Fileextension: </span>
+        <input
+          id="rd-png"
+          type="radio"
+          value="png"
+          checked={fileextension === "png"}
+          onChange={onExtensionChange}
+        />
+        <label htmlFor="rd-png">png</label>
+        <input
+          style={{ marginLeft: "1rem" }}
+          id="rd-pdf"
+          type="radio"
+          value="pdf"
+          checked={fileextension === "pdf"}
+          onChange={onExtensionChange}
+        />
+        <label htmlFor="rd-pdf">pdf</label>
+        <button
+          className="btn-export"
+          onClick={exportTo}
+          style={{ marginLeft: "2rem" }}
+        >
           Export
         </button>
       </section>
